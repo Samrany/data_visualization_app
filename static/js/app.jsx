fc1e2f3d0aa0a66f83ba07202e33fbf748b70184
Shira Amrany <shira.amrany@gmail.com>
2020-07-21 00:21:07 +0000

    initial component and method

---
+++ static/js/app.jsx
@@ -0,0 +1,44 @@
+"use strict";
+
+class App extends React.Component{
+	constructor(){
+		super();
+		this.state={
+			locationChosen: ""
+
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(evt){
+		this.setState({evt.target.id: evt.target.value});
+		}
+
+
+
+
+	render(){
+		return (
+			<div> 
+				<form>
+					<select id="locationChosen" placeholder="choose location" value={this.state.locationChosen} onChange={this.handleChange}> 
+						<option value="locationA"> Location A </option>
+						<option value="locationB"> Location B </option>
+						<option value="locationC"> Location C </option>				
+					</select>
+
+				</form>
+
+
+			</div>
+			)
+	}
+
+}
+
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#app')
+);
